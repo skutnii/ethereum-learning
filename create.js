027510b9f5204b25d15d615ed3dafb25d27dd56f
Sergii Kutnii <mnkutster@gmail.com>
2017-10-08 23:35:43 +0300

    Password confirmation added

--- create.js
+++ create.js
@@ -2,13 +2,22 @@ var testnet = require('./testnet');
 var input = require('./input');
 
 var file = "";
+var pass1 = "";
 input.ask("Enter destination file path.\n")
 .then(function(answer) {
 	file = answer;
 	return input.askPrivate("Enter encryption password.\n");
 })
 .then(function(pass) {
+	pass1 = pass;
+	return input.askPrivate("Repeat encryption password.\n");
+})
+.then(function(pass) {
 	try {
+		if (pass1 !== pass) {
+			throw "Passwords do not match.";
+		}
+
 		var account = testnet.eth.accounts.create();
 
 		var aes256 = require('nodejs-aes256');
